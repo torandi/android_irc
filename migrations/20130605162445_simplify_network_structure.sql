c9ed1617e5de8c75626a509370bad5b62062e2d7
Andreas Tarandi <torandi@gmail.com>
2013-06-07 15:21:10 +0200

    Herp derp wrong default port

--- migrations/20130605162445_simplify_network_structure.sql
+++ migrations/20130605162445_simplify_network_structure.sql
@@ -3,7 +3,7 @@ ALTER TABLE `user_networks` DROP FOREIGN KEY `user_networks_ibfk_2`;
 ALTER TABLE `user_networks` DROP COLUMN `network_id`;
 
 ALTER TABLE `user_networks` ADD `address` VARCHAR( 128 ) NOT NULL ,
-ADD `port` INT NOT NULL DEFAULT 6669;
+ADD `port` INT NOT NULL DEFAULT 6667;
 
 DROP TABLE servers;
 DROP TABLE networks;
