156bc5f75b4b75cba8300ffdcd2f69ffcc28ed2a
Perka <perka@home.com>
2013-06-07 21:10:58 +0200

    Built a GUI mockup skeleton
    TODO:
    Add functions on button presses
    Fix TODO defenitions
    Style the UI
    Fix double icon icon in the action bar
    
    The ic_drawer.png is from the standard libs

--- android_client/src/com/example/andriod_irc_frontend/MainActivity.java
+++ android_client/src/com/example/andriod_irc_frontend/MainActivity.java
@@ -2,14 +2,81 @@ package com.example.andriod_irc_frontend;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private String[] mButtonTiles;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Fetch the string array, values/strings.xml
+		mButtonTiles = getResources().getStringArray(R.array.example_buttons);
+		// Left drawer,see activity_main.xml
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, mButtonTiles));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// Click the icon to view bar
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, // Icon
+				R.string.drawer_open, // Tool tip
+				R.string.drawer_close) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				// TODO Stuff?
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				// TODO Stuff?
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		// So we can click the actionbar to view the drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	// Varför denna meny?
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
@@ -19,4 +86,19 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		//TODO fix unsafe 
+		public void onItemClick(AdapterView parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+	}
+
+	private void selectItem(int position) {
+		// TODO handle clicks properly
+		mDrawerList.setItemChecked(position, true); // Highlight item
+		mDrawerLayout.closeDrawer(mDrawerList); // Close the drawer
+	}
 }
