c1093fb61d00f8e4a36961ee2d47e18e889b9fae
Andreas Tarandi <torandi@gmail.com>
2013-06-07 13:33:43 +0200

    Simplify network structure

---
+++ migrations/20130605162445_simplify_network_structure.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `user_networks` DROP FOREIGN KEY `user_networks_ibfk_2`;
+
+ALTER TABLE `user_networks` DROP COLUMN `network_id`;
+
+ALTER TABLE `user_networks` ADD `address` VARCHAR( 128 ) NOT NULL ,
+ADD `port` INT NOT NULL DEFAULT 6669;
+
+DROP TABLE servers;
+DROP TABLE networks;
+
+
